a10f89afc6aa5abbd074d5744fabd65441c8e32a
Ivan Sergeev <49801117+Tw1ster95@users.noreply.github.com>
2022-06-09 11:06:07 +0300

    Update cloudinary-sdk-usage.php

--- php/cloudinary-sdk-usage.php
+++ php/cloudinary-sdk-usage.php
@@ -1,15 +1,17 @@
-use Cloudinary\Cloudinary;
+<?php
+    use Cloudinary\Cloudinary;
 
-$cloudinary = new Cloudinary([
-    'cloud' => [
-        'cloud_name' => '', 
-        'api_key' => '', 
-        'api_secret' => ''
-    ],
-    'url' => [
-        'secure' => true
-        ]
-    ]);
+    $cloudinary = new Cloudinary([
+        'cloud' => [
+            'cloud_name' => '', 
+            'api_key' => '', 
+            'api_secret' => ''
+        ],
+        'url' => [
+            'secure' => true
+            ]
+        ]);
 
-$imageObj = $cloudinary->uploadApi()->upload($_FILES['file']['tmp_name'], ["public_id" => $new_filename, "folder" => $DossierI]);
-$url = $imageObj['secure_url'];
+    $imageObj = $cloudinary->uploadApi()->upload($_FILES['file']['tmp_name'], ["public_id" => $new_filename, "folder" => $DossierI]);
+    $url = $imageObj['secure_url'];
+?>
