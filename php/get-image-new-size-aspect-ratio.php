86cedd347929e3a9f601ae6fd8f763b91cdf6ede
Ivan Sergeev <49801117+Tw1ster95@users.noreply.github.com>
2022-06-09 11:06:28 +0300

    Update get-image-new-size-aspect-ratio.php

--- php/get-image-new-size-aspect-ratio.php
+++ php/get-image-new-size-aspect-ratio.php
@@ -1,12 +1,12 @@
 <?php
-function getFixedAspectRatio($image, $max_height, $max_width) {
-    list($width, $height, $type, $attr) = getimagesize($image);
-    $new_height = $max_height;
-    $new_width = intval(($width / $height) * $new_height);
-    if($new_width > $max_width) {
-        $new_width = $max_width;
-        $new_height = intval(($height / $width) * $new_width);
+    function getFixedAspectRatio($image, $max_height, $max_width) {
+        list($width, $height, $type, $attr) = getimagesize($image);
+        $new_height = $max_height;
+        $new_width = intval(($width / $height) * $new_height);
+        if($new_width > $max_width) {
+            $new_width = $max_width;
+            $new_height = intval(($height / $width) * $new_width);
+        }
+        return [ $new_width, $new_height ];
     }
-    return [ $new_width, $new_height ];
-}
 ?>
